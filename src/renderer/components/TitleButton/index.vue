b723ba507d5fc392e7e61b0889750e2e9d58288f
e <1510748736@qq.com>
2020-03-07 19:41:21 +0800

    1. 添加最小化、最大化、关闭按钮模板

---
+++ src/renderer/components/TitleButton/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="titlebtn"
+    :style="style"
+    @click="send"
+  />
+</template>
+
+<script>
+const style = {
+  min: {
+    backgroundColor: 'green',
+    right: '65px'
+  },
+  max: {
+    backgroundColor: 'yellow',
+    right: '40px'
+  },
+  close: {
+    backgroundColor: 'red',
+    right: '15px'
+  }
+}
+
+export default {
+  name: 'TitleButton',
+  props: ['type'],
+  computed: {
+    style: function() {
+      return style[this.type]
+    }
+  },
+  methods: {
+    send: function() {
+      const { ipcRenderer } = require('electron')
+      ipcRenderer.send(this.type)
+    }
+  }
+}
+</script>
+
+<style>
+  .titlebtn {
+    position: absolute;
+    top: 10px;
+    width: 15px;
+    height: 15px;
+    -webkit-app-region: no-drag;
+    border-radius: 50%;
+    z-index: 10;
+  }
+  .titlebtn:hover{
+    border: 1px solid rgba(248, 242, 242, 0.6);
+  }
+</style>
