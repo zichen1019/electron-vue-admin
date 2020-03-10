fe920142151726343c074b50d474f4b8f00ee788
e <1510748736@qq.com>
2020-03-10 21:52:29 +0800

    add===>: 1. 加入登录功能。

--- src/renderer/store/modules/user.js
+++ src/renderer/store/modules/user.js
@@ -4,20 +4,69 @@ import { getToken, setToken, removeToken } from '@/utils/auth'
 const user = {
   state: {
     token: getToken(),
+    userId: '',
+    status: '',
+    code: '',
     name: '',
     avatar: '',
-    roles: []
+    introduction: '',
+    birday: '',
+    gender: '',
+    addres: '',
+    mobile: '',
+    roles: [],
+    setting: {
+      articlePlatform: []
+    },
+    ip: ''
   },
 
   mutations: {
+    SET_USER: (state, user) => {
+      state.userId = user.id
+      state.name = user.nickname
+      state.avatar = user.image ? user.image : 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif'
+      state.introduction = user.nickname
+      state.birday = user.birday
+      state.gender = user.gender
+      state.addres = user.addres
+      state.mobile = user.mobile
+      state.roles = user.roles
+    },
+    NULL_USER: (state) => {
+      state.userId = ''
+      state.name = ''
+      state.avatar = 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif'
+      state.introduction = ''
+      state.birday = ''
+      state.gender = ''
+      state.addres = ''
+      state.mobile = ''
+      state.roles = []
+    },
     SET_TOKEN: (state, token) => {
       state.token = token
+    },
+    SET_CODE: (state, code) => {
+      state.code = code
+    },
+    SET_INTRODUCTION: (state, introduction) => {
+      state.introduction = introduction
+    },
+    SET_SETTING: (state, setting) => {
+      state.setting = setting
+    },
+    SET_STATUS: (state, status) => {
+      state.status = status
     },
     SET_NAME: (state, name) => {
       state.name = name
     },
     SET_AVATAR: (state, avatar) => {
       state.avatar = avatar
+    },
+    SET_IP: (state, ip) => {
+      state.ip = ip
     },
     SET_ROLES: (state, roles) => {
       state.roles = roles
@@ -29,8 +78,7 @@ const user = {
     Login({ commit }, userInfo) {
       const username = userInfo.username.trim()
       return new Promise((resolve, reject) => {
-        login(username, userInfo.password).then(response => {
-          const data = response.data
+        login(username, userInfo.password).then(data => {
           setToken(data.token)
           commit('SET_TOKEN', data.token)
           resolve()
@@ -41,18 +89,20 @@ const user = {
     },
 
     // 获取用户信息
-    GetInfo({ commit, state }) {
+    GetInfo({ commit }) {
       return new Promise((resolve, reject) => {
-        getInfo(state.token).then(response => {
-          const data = response.data
-          if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
-            commit('SET_ROLES', data.roles)
+        getInfo().then(response => {
+          const { user } = response
+          if (!user) {
+            reject('Verification failed, please Login again.')
+          }
+          if (user.roles && user.roles.length > 0) { // 验证返回的roles是否是一个非空数组
+            commit('SET_ROLES', user.roles)
           } else {
             reject('getInfo: roles must be a non-null array !')
           }
-          commit('SET_NAME', data.name)
-          commit('SET_AVATAR', data.avatar)
-          resolve(response)
+          commit('SET_USER', user)
+          resolve(user)
         }).catch(error => {
           reject(error)
         })
--- src/renderer/utils/request.js
+++ src/renderer/utils/request.js
@@ -12,6 +12,8 @@ const service = axios.create({
 service.interceptors.request.use(config => {
   if (store.getters.token) {
     config.headers['X-Token'] = store.getters.token// 让每个请求携带自定义token 请根据实际情况自行修改
+  } else {
+    config.headers['x-csrftoken'] = 'admin-token'
   }
   return config
 }, error => {
@@ -27,7 +29,7 @@ service.interceptors.response.use(
   * code为非20000是抛错 可结合自己业务进行修改
   */
     const res = response.data
-    if (res.code !== 20000) {
+    if (res.StatusCode !== 200) {
       Message({
         message: res.message,
         type: 'error',
@@ -35,7 +37,7 @@ service.interceptors.response.use(
       })
 
       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.StatusCode === 50008 || res.StatusCode === 50012 || res.StatusCode === 50014) {
         MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
           confirmButtonText: '重新登录',
           cancelButtonText: '取消',
